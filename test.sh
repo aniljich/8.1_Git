41dcc8552dc5e65a70175c724c5d6841bc3f5d1b
Andrei Korotkov <admin@koshkindom.ee>
2025-04-27 19:58:49 +0300

    Edited test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,10 @@
 #/bin/bash
+
+declare -i PORT=80
+declare -a INDEX="/var/www/html/index.html"
+
+if { nc -z localhost "$PORT" && [ -f "$INDEX" ]; } > /dev/null 2>&1; then
+  exit 0
+else
+  exit 1
+fi
